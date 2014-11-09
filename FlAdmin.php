6f7a9e4dbe288dd935cafd8fccb22c77835bb577
Artem Belov <otsec.ru@gmail.com>
2014-11-09 19:19:19 +0300

    Refactored FlAdmin panel generator

--- FlAdmin.php
+++ FlAdmin.php
@@ -11,56 +11,25 @@ use yii\helpers\Html;
 class FlAdmin
 {
     /**
-     * @param string  $header
-     * @param boolean $wrapContent
+     * @param string $header
      * @return string
      */
-    public static function beginPanel($header = null, $wrapContent = false)
+    public static function beginPanel($header = null)
     {
-        $output = Html::beginTag('div', ['class' => 'panel']);
+        $panel = Html::beginTag('div', ['class' => 'panel']);
+        $body  = Html::beginTag('div', ['class' => 'panel-body']);
 
         if ($header) {
-            $output .= Html::tag('div', $header, ['class' => 'panel-heading']);
+            $header = Html::tag('div', $header, ['class' => 'panel-heading']);
         }
 
-        $output .= Html::beginTag('div', ['class' => 'panel-body']);
-
-        if ($wrapContent) {
-            $output .= static::beginWrapper();
-        }
-
-        return $output;
+        return $panel . "\n" . $header . "\n" . $body;
     }
 
     /**
      * @return string
      */
-    public static function beginWrapper()
-    {
-        return
-            Html::beginTag('div', ['class' => 'form-group']) .
-            Html::beginTag('div', ['class' => 'col-sm-offset-3 col-sm-9 col-md-offset-2 col-md-10']);
-    }
-
-    /**
-     * @param boolean $wrapContent
-     * @return string
-     */
-    public static function endPanel($wrapContent = false)
-    {
-        $output = str_repeat(Html::endTag('div'), 2);
-
-        if ($wrapContent) {
-            $output .= static::endWrapper();
-        }
-
-        return $output;
-    }
-
-    /**
-     * @return string
-     */
-    public static function endWrapper()
+    public static function endPanel()
     {
         return str_repeat(Html::endTag('div'), 2);
     }
